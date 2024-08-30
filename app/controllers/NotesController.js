21fa73c5a3a6f491722098dec9bd72a6d1cf2df2
EllyCasey <ellia.casey@gmail.com>
2024-08-30 15:25:17 -0600

    Test note from AppState Array now draws. Next is to make notesList and make it draw

--- app/controllers/NotesController.js
+++ app/controllers/NotesController.js
@@ -1,6 +1,16 @@
+import { AppState } from "../AppState.js";
+import { setHTML } from "../utils/Writer.js"
 
 export class NotesController {
     constructor() {
-        console.log('Notes Controller is loaded')
+        console.log('Notes Controller is loaded');
+        this.drawActiveNote()
+    }
+
+    drawActiveNote() {
+        const notes = AppState.notes
+        let userNotes = '';
+        notes.forEach((note) => userNotes += note.notesHTMLTemplate)
+        setHTML('userNote', userNotes)
     }
 }
--- app/models/Note.js
+++ app/models/Note.js
@@ -10,4 +10,17 @@ export class Note {
         this.createdAt = data.createdAt //NOTE this will eventually change to a data function I believe
         this.updatedAt = data.updatedAt //NOTE same as above, this should be a date function eventually
     }
+
+    get notesHTMLTemplate() {
+        return /*html*/ `
+        <div class="col-7 list-box me-2">
+            <h3>${this.title}</h3>
+            <p>${this.createdAt}</p>
+            <p>${this.updatedAt}</p>
+            <label for="userNote"></label>
+            <input type="textarea" class="textarea" placeholder="${this.body}">
+            <p class="d-flex justify-content-end">0 Words</p>
+      </div>
+        `
+    }
 }
